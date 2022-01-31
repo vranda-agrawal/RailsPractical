6f3f5c98172d150475e142177cbb3382f6fb2e5b
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-01-31 19:41:22 +0530

    Active record callback practice

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,15 +3,35 @@ class Student < ApplicationRecord
     validates :department, inclusion: { in: %w(IT CE),
         message: "can't be %{value}" }
     validates :Terms_Of_Condition, acceptance: { message: 'must be abided' }
-    # t=Time.new
-    # t=t.strftime("%d/%m/%y")
-    # validates :DOB,comparison: {less_than_or_equal_to: t,message:"birthday can't be in future"}
-    # # validate :DOB_cannot_be_in_the_future
-
-    # def DOB_cannot_be_in_the_future
-    #     if DOB.present? && DOB > Date.today
-    #       errors.add(:DOB, "can't be in the future")
-    #     end
-    #   end
+    before_validation :ensure_last_name_has_a_value
+    after_validation :change_value_of_the_departement
     
+    #initialize callback
+    
+    after_initialize do |u|
+        puts "You have initialized an object! #{u}"
+      end
+    
+    #touch call back
+    after_touch do |user|
+      puts "You have touched an object"
+    end
+    
+    private
+      def ensure_last_name_has_a_value
+        if last_name.nil?
+            puts "last_name callback called"
+            self.last_name="-"
+        end
+      end
+      def change_value_of_the_departement
+        if department == 'IT'
+            puts "last_name callback called"
+            self.department="Information technology"
+        end
+        if department == 'CE'
+            puts "last_name callback called"
+            self.department="Computer"
+        end
+      end
 end
