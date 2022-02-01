86540e33a5166358147d9fe428b76ce0bbc49578
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-01 12:33:18 +0530

    Code improved as suggested by mentor

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -1,65 +1,60 @@
 class FacultiesController < ApplicationController
-    def index
-        @faculties = Faculty.all
-      end
-    
-    def show
-          puts "----------------------------------------------------------------------"
-          puts params
-          puts params['id']
-          @faculty= Faculty.find(params['id'])
-          puts @faculty.first_name
-          puts "-----------------------------------------------------------------------"
-      end
-    
-    def new
-        @faculty = Faculty.new
-      end
-
-    def create
-      @faculty=Faculty.create(faculty_params)
-      if @faculty.valid?
-        puts '---------------valid user----------------'
-        redirect_to '/faculties'
-      else 
-        puts '-----------------not valid-----------------'
-        # flash[:errors] = student.errors.full_messages
-        puts @faculty.errors.full_messages
-        puts @faculty.errors.any?
-        render 'new'
-      end
+  def index
+      @faculties = Faculty.all
     end
-    
-
-    def edit
-    puts"--------------------------------------------------------"
+  
+  def show
     @faculty= Faculty.find(params['id'])
-    end
-    
-    def update
-    @faculty = Faculty.find(params[:id])
-    @faculty.update(faculty_params)
+  end
+  
+  def new
+    @faculty = Faculty.new
+  end
+
+  def create
+    @faculty=Faculty.create(faculty_params)
     if @faculty.valid?
-        puts '---------------valid user----------------'
-        redirect_to '/faculties'
-      else 
-        puts '-----------------not valid-----------------'
-        puts @faculty.errors.full_messages
-        puts @faculty.errors.any?
-        render 'edit'
-      end
-    
+      #puts '---------------valid user----------------'
+      redirect_to '/faculties'
+    else 
+      #puts '-----------------not valid-----------------'
+      # flash[:errors] = student.errors.full_messages
+      #puts @faculty.errors.full_messages
+      #puts @faculty.errors.any?
+      render 'new'
     end
+  end
+  
 
-      def destroy
-        puts "============================================================================="
-        @faculty = Faculty.find(params[:id])
-        @faculty.destroy
-        redirect_to '/faculties'
-      end
-
-    private
-    def faculty_params
-        params.require(:faculty).permit(:first_name,:last_name,:DOB,:phone_number,:email,:designation)
+  def edit
+  #puts"--------------------------------------------------------"
+  @faculty= Faculty.find(params['id'])
+  end
+  
+  def update
+  @faculty = Faculty.find(params[:id])
+  @faculty.update(faculty_params)
+  if @faculty.valid?
+      #puts '---------------valid user----------------'
+      redirect_to '/faculties'
+    else 
+      #puts '-----------------not valid-----------------'
+      #puts @faculty.errors.full_messages
+      #puts @faculty.errors.any?
+      render 'edit'
     end
+  
+  end
+
+  def destroy
+    #puts "============================================================================="
+    @faculty = Faculty.find(params[:id])
+    @faculty.destroy
+    redirect_to '/faculties'
+  end
+
+  private
+  def faculty_params
+      params.require(:faculty).permit(:first_name,:last_name,:DOB,:phone_number,:email,:designation)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,66 +1,60 @@
 class StudentsController < ApplicationController
-    def index
-        @students = Student.all
-      end
-    
-    def show
-          puts "----------------------------------------------------------------------"
-          puts params
-          puts params['id']
-          @student= Student.find(params['id'])
-          puts @student.first_name
-          puts "-----------------------------------------------------------------------"
-      end
-    
-    def new
-        @student = Student.new
-      end
-
-    def create
-      @student=Student.create(student_params)
-      if @student.valid?
-        puts '---------------valid user----------------'
-        redirect_to '/students'
-      else 
-        puts '-----------------not valid-----------------'
-        # flash[:errors] = student.errors.full_messages
-        puts @student.errors.full_messages
-        puts @student.errors.any?
-        render 'new'
-      end
-    end
-    
-
-    def edit
-    puts"--------------------------------------------------------"
+  def index
+    @students = Student.all
+  end
+  
+  def show
     @student= Student.find(params['id'])
+  end
+  
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student=Student.create(student_params)
+    if @student.valid?
+      #puts '---------------valid user----------------'
+      redirect_to '/students'
+    else 
+      #puts '-----------------not valid-----------------'
+      # flash[:errors] = student.errors.full_messages
+      #puts @student.errors.full_messages
+      #puts @student.errors.any?
+      render 'new'
     end
-    
-    def update
+  end
+  
+
+  def edit
+    @student= Student.find(params['id'])
+  end
+  
+  def update
     @student = Student.find(params[:id])
     @student.update(student_params)
     if @student.valid?
-        puts '---------------valid user----------------'
-        redirect_to students_path
-      else 
-        puts '-----------------not valid-----------------'
-        puts @student.errors.full_messages
-        puts @student.errors.any?
-        render 'edit'
-      end
-    
+      #puts '---------------valid user----------------'
+      redirect_to students_path
+    else 
+      #puts '-----------------not valid-----------------'
+      #puts @student.errors.full_messages
+      #puts @student.errors.any?
+      render 'edit'
     end
+  
+  end
 
-      def destroy
-        puts "============================================================================="
-        @student = Student.find(params[:id])
-        @student.destroy
-        redirect_to '/students'
-      end
+  def destroy
+    puts "============================================================================="
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to '/students'
+  end
 
-    private
-    def student_params
-        params.require(:student).permit(:first_name,:last_name,:DOB,:department,:Terms_Of_Condition)
-    end
-    
+  private
+  def student_params
+      params.require(:student).permit(:first_name,:last_name,:DOB,:department,:Terms_Of_Condition)
+  end
+  
 end
