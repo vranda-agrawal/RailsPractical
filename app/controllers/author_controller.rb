fe6b8710777e15f996b8d0da1f0f2b5a04435f42
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-01 12:19:15 +0530

    Indentated code according to the mentor's suggestion

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,59 +1,48 @@
 class AuthorController < ApplicationController
-            # GET /products or /products.json
-            def index
-                @authors = Author.all
-              end
-            
-              # # GET /products/1 or /products/1.json
-              def show
-                  puts "----------------------------------------------------------------------"
-                  puts params
-                  puts params['id']
-                  @author= Author.find(params['id'])
-                  puts @author.first_name
-                  puts "-----------------------------------------------------------------------"
-              end
-            
-              # # GET /products/new
-              def new
-                 @author = Author.new
-               end
-            
-              # # GET /products/1/edit
-              def edit
-                puts "----------------------------------------------------------------------------------------------------------"
-                puts request
-                puts params
-                @author= Author.find(params['id'])
-                puts request.method
-              end
-            
-              # # POST /products or /products.json
-              def save
-                puts "================================================================="
-                #puts request.method
-                puts params
-                @author=Author.create(params.permit(:first_name,:last_name,:DOB,:email))
-                if @author.valid?
-                  puts '---------------valid user----------------'
-                  redirect_to '/author'
-                else 
-                  puts '-----------------not valid-----------------'
-                  redirect_to '/author/new'
-                end
-              end
-              def update
-                @author = Author.find(params[:id])
-                @author.update(book_params)
-                redirect_to author_path
-              end
-              def book_params
-                params.require(:author).permit(:first_name,:last_name,:DOB,:email)
-              end
-              def destroy
-                puts "============================================================================="
-                @author = Author.find(params[:id])
-                @author.destroy
-              end
+  # GET /products or /products.json
+  def index
+    @authors = Author.all
+  end
+
+  # GET /products/1 or /products/1.json
+  def show
+      @author= Author.find(params['id'])
+      
+  end
+
+  # GET /products/new
+  def new
+      @author = Author.new
+    end
+
+  # GET /products/1/edit
+  def edit
+    @author= Author.find(params['id'])
+  end
+
+  # POST /products or /products.json
+  def save
+    @author=Author.create(params.permit(:first_name,:last_name,:DOB,:email))
+    if @author.valid?
+      redirect_to '/author'
+    else 
+      redirect_to '/author/new'
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(book_params)
+    redirect_to author_path
+  end
+
+  def book_params
+    params.require(:author).permit(:first_name,:last_name,:DOB,:email)
+  end
+  
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+  end
       
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,61 +1,47 @@
 class ImagesController < ApplicationController
+  # GET /products or /products.json
+  def index
+    @images = Image.all
+  end
 
+  # GET /products/1 or /products/1.json
+  def show
+      @image= Image.find(params['id'])
+  end
 
-        # GET /products or /products.json
-        def index
-            @images = Image.all
-          end
-        
-          # # GET /products/1 or /products/1.json
-          def show
-              puts "----------------------------------------------------------------------"
-              puts params
-              puts params['id']
-              @image= Image.find(params['id'])
-              puts @image.img_name
-              puts "-----------------------------------------------------------------------"
-          end
-        
-          # # GET /products/new
-          def new
-             @image = Image.new
-           end
-        
-          # # GET /products/1/edit
-          def edit
-            puts "----------------------------------------------------------------------------------------------------------"
-            puts request
-            puts params
-            @image= Image.find(params['id'])
-            puts request.method
-          end
-        
-          # # POST /products or /products.json
-          def save
-            puts "================================================================="
-            #puts request.method
-            puts params
-            @image=Image.create(params.permit(:img_name,:author_id,:book_id))
-            if @image.valid?
-              puts '---------------valid user----------------'
-              redirect_to '/images'
-            else 
-              puts '-----------------not valid-----------------'
-              redirect_to '/images/new'
-            end
-          end
-          def update
-            @image = Image.find(params[:id])
-            @image.update(book_params)
-            redirect_to images_path
-          end
-          def book_params
-            params.require(:image).permit(:img_name, :author_id,:book_id)
-          end
-          def destroy
-            puts "============================================================================="
-            @image = Image.find(params[:id])
-            @image.destroy
-          end
-          
+  # GET /products/new
+  def new
+      @image = Image.new
+    end
+
+  # GET /products/1/edit
+  def edit
+    @image= Image.find(params['id'])
+  end
+
+  # POST /products or /products.json
+  def save
+    @image=Image.create(params.permit(:img_name,:author_id,:book_id))
+    if @image.valid?
+      redirect_to '/images'
+    else 
+      redirect_to '/images/new'
+    end
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update(book_params)
+    redirect_to images_path
+  end
+
+  def book_params
+    params.require(:image).permit(:img_name, :author_id,:book_id)
+  end
+  
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+  end
+  
 end
