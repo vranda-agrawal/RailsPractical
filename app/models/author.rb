8a3ce2f666557059bcafa13b1d77babd709508bc
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-03 14:42:05 +0530

    Proper indentation added

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,9 @@
 class Author < ApplicationRecord
-	validates :first_name, presence: true, length: {maximum: 15}
-	validates :last_name, presence: true, length: {maximum: 15}
-	validates :email, presence: true , uniqueness: true 
-	
-	has_many :books
-	has_many :images
-	
+  validates :first_name, presence: true, length: {maximum: 15}
+  validates :last_name, presence: true, length: {maximum: 15}
+  validates :email, presence: true , uniqueness: true 
+  
+  has_many :books
+  has_many :images
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-	belongs_to :author
-	has_many :images
+  belongs_to :author
+  has_many :images
 end
