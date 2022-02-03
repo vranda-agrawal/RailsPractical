30484506364e791f3eba637589ee0fc87b30b275
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-03 15:07:01 +0530

    Proper Indentation added

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -2,10 +2,7 @@ class Faculty < ApplicationRecord
     validates :first_name,:last_name, presence: true
     validates :phone_number, format: {with: /\A[0-9]{10}\z/ }
     validates :email, presence: true, uniqueness: true
-    validates :designation, exclusion: { in: %w(HOD Sr.Prof.) },inclusion: { in: %w(Ass.Prof. Prof) ,
-        message: "can't be %{value}" }
-    t=Time.new
-    t=t.strftime("%Y-%m-%d")
-    validates :DOB,comparison: {less_than_or_equal_to: t,message:"birthday can't be in future"}
+    validates :designation, exclusion: { in: %w(HOD Sr.Prof.) },inclusion: { in: %w(Ass.Prof. Prof) , message: "can't be %{value}" }
+    validates :DOB,comparison: {less_than_or_equal_to: Date.today,message:"birthday can't be in future"}
     
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,19 +1,7 @@
 class Student < ApplicationRecord
     validates :first_name, :last_name , presence: true
-    validates :department, inclusion: { in: %w(IT CE),
-        message: "can't be %{value}" }
+    validates :department, inclusion: { in: %w(IT CE), message: "can't be %{value}" }
     validates :Terms_Of_Condition, acceptance: { message: 'must be abided' }
-    t=Time.new
-    t=t.strftime("%Y-%m-%d")
     validates :DOB,comparison: {less_than_or_equal_to: Date.today,message:"birthday can't be in future"}
-    
-
-    # validate :DOB_cannot_be_in_the_future
-
-    # def DOB_cannot_be_in_the_future
-    #     if DOB.present? && DOB > Date.today
-    #       errors.add(:DOB, "can't be in the future")
-    #     end
-    #   end
     
 end
