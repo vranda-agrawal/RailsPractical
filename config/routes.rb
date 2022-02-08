6dd1a49085c458c14f962d16d4dbacc13d296c9d
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-08 09:18:49 +0530

    Created Employee Model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :books
   resources :author
   resources :images
+  resources :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/books/save', to: 'books#save', as: 'save'
   post '/author/save', to: 'author#save', as: 'author_save' 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_150717) do
+ActiveRecord::Schema.define(version: 2022_02_04_130157) do
+
+  create_table "Students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "DOB", precision: 6
+    t.string "department"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "Terms_Of_Condition"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -30,12 +40,35 @@ ActiveRecord::Schema.define(version: 2022_01_25_150717) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "age"
+    t.integer "no_of_order"
+    t.boolean "full_time_available"
+    t.float "salary"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "DOB", precision: 6
+    t.integer "phone_number"
+    t.string "email"
+    t.string "designation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "images", force: :cascade do |t|
     t.string "img_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id", null: false
-    t.integer "book_id", null: false
+    t.integer "book_id"
     t.index ["author_id"], name: "index_images_on_author_id"
     t.index ["book_id"], name: "index_images_on_book_id"
   end
