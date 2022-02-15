633acf3bb7f8c18abc129a03bb8bfb6ff126ae0b
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-15 16:42:51 +0530

    Fix : Routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,7 @@ Rails.application.routes.draw do
   resources :books
   resources :author
   resources :images
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post '/books/save', to: 'books#save', as: 'save'
-  post '/author/save', to: 'author#save', as: 'author_save' 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :books
-  resources :author
-  resources :images
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/books/save', to: 'books#save', as: 'save'
   post '/author/save', to: 'author#save', as: 'author_save' 
