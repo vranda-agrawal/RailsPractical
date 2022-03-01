474f1b20b41916cfaee1cd4d4bae80e2c9c24526
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-03-01 11:25:36 +0530

    Added logout button at home page and removed puts statement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 class HomeController < ApplicationController
   before_action :determine_layout
   layout :determine_layout
+  
   def index
-    puts current_my_user
+  
   end
 
   private
   def determine_layout
-    puts"--------------------------checking user role------------------------------------------"
-    puts current_my_user
     if current_my_user.role=='admin'
       page_layout="admin"
     else
