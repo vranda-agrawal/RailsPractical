2258e9fdba1c20c4169f648bc2bc28097ffb340c
vranda-agrawal <97445991+vranda-agrawal@users.noreply.github.com>
2022-03-11 09:02:04 +0530

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,25 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::IntegrationHelpers, type: :request
+#sessions_spec  
+#   it "authenticate member" do
+#     create(:my_user)
+#     @request.env["devise.mapping"] = Devise.mappings[:my_user]
+#     @request.env["warden"] = warden
+
+#     ## Added post request to "create"
+#     post :create, :my_user => FactoryBot.attributes_for(:my_user)
+
+#     ## Added new expectations
+#     expect(controller.myuser_signed_in?).to be true
+#     expect(controller.current_my_user.email).to eq "lp@gmail.com"
+
+#     ## "session["warden.user.member.key"][0].first" stores the "id" of logged in member
+#     ## Replace "member" in "session["warden.user.member.key"][0].first" with your
+#     ## devise model name. For example: If your devise model name is customer then you
+#     ## need to check "session["warden.user.customer.key"][0].first"
+#     expect(session["warden.user.my_user.key"][0].first).to eq member.id
+end
+
 end
