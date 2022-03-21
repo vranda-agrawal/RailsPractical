6f3170b9c0175cd05002070a7fac9f56417c3050
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-03-21 14:02:24 +0530

    removed user test cases

--- spec/controller/my_user_request_spec.rb
+++ spec/controller/my_user_request_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe MyUser, type: :request do
-  it "gets all users" do
-      get my_user_index_path
-      expect(response).to be_successful
-  end
-
+  
 end 
