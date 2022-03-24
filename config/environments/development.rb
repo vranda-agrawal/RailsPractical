5ea3ebd1cf58dc4ee8d5fc4600554ac9fcf567ab
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-03-24 12:41:04 +0530

    Removed api configuration from an app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,10 +55,9 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  config.api_only = false
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.debug_exception_response_format = :api
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
